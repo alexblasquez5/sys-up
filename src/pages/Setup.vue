255f013793685cb9610d1240c81f17cfb72639b2
Alejandro Blasquez <alexb@phi-tech.net>
2026-04-08 13:19:31 -0600

    Rebranded title to PhiGuard Sys Up and changed the primary color to #00B3FE

--- src/pages/Setup.vue
+++ src/pages/Setup.vue
@@ -3,8 +3,8 @@
         <div class="form">
             <form @submit.prevent="submit">
                 <div>
-                    <object width="64" height="64" data="/icon.svg" />
-                    <div style="font-size: 28px; font-weight: bold; margin-top: 5px">Uptime Kuma</div>
+                    <img src="/sys-up-logo.png" width="64" height="64" style="object-fit: contain;" />
+                    <div style="font-size: 28px; font-weight: bold; margin-top: 5px">PhiGuard SysUp</div>
                 </div>
 
                 <p class="mt-3">
