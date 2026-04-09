069e6a206b65431b6d994e250eae95fe58fc8670
Alejandro Blasquez <alexb@phi-tech.net>
2026-04-09 08:04:36 -0600

    Changed header color and added a small bottom border to it

--- src/layouts/Layout.vue
+++ src/layouts/Layout.vue
@@ -13,7 +13,7 @@
         </div>
 
         <!-- Desktop header -->
-        <header v-if="!$root.isMobile" class="d-flex flex-wrap justify-content-center py-3 mb-3 border-bottom">
+        <header v-if="!$root.isMobile" class="d-flex flex-wrap justify-content-center py-3 mb-3">
             <router-link
                 to="/dashboard"
                 class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none"
